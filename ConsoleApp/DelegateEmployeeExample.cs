0b4159054f2422c5647de9323e872ab2cd69766f
Adithyavj <adithyavj@gmail.com>
2020-08-06 22:25:29 +0530

    standard example of delegate usage

---
+++ ConsoleApp/DelegateEmployeeExample.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class DelegateEmployeeExample
+    {
+        static void main()
+        {
+            List<Employee> empList = new List<Employee>();
+
+            empList.Add(new Employee() { ID = 101, Name = "Adi", Salary = 20000, Experience = 2 });
+            empList.Add(new Employee() { ID = 102, Name = "Fasil", Salary = 25000, Experience = 5 });
+            empList.Add(new Employee() { ID = 103, Name = "Jishnu", Salary = 10000, Experience = 2 });
+            empList.Add(new Employee() { ID = 104, Name = "Ashraf", Salary = 20000, Experience = 6 });
+
+            //instance of the delegate
+            IsPromotable isPromotable = new IsPromotable(Promote);
+
+            //calling the function
+            Employee.PromoteEmployee(empList, isPromotable);
+        }
+
+
+        //This is the function which does the logic for us
+        //It has the same signature as that of the delegate
+        public static bool Promote(Employee emp)
+        {
+            if (emp.Experience >= 5)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    delegate bool IsPromotable(Employee empl);
+
+    //we make this class reusable
+    //depending on the need the method should be flexible
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        //Method to check if employee is promoted
+        public static void PromoteEmployee(List<Employee> employeeList, IsPromotable IsEligibleToPromote) // to passin a function as a parameter use a delegate
+        {
+            foreach (Employee employee in employeeList)
+            {
+                //here the logic is hardcoded, to replace this and make it reusable
+                // we use delegate
+                //if (employee.Experience >= 5)
+                //{
+                //    Console.WriteLine(employee.Name + " Promoted");
+                //}
+
+                //We replace the old code with a delegate
+                if (IsEligibleToPromote(employee))
+                {
+                    Console.WriteLine(employee.Name + " Promoted");
+                }
+            }
+        }
+    }
+}
